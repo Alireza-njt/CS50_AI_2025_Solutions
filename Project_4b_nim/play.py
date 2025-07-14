edf672491568a7f7ecc6294bbf3985b5d502801a
Alireza Nejati <121515026+Alireza-njt@users.noreply.github.com>
2025-07-15 02:56:59 +0330

    Update play.py

--- Project_4b_nim/play.py
+++ Project_4b_nim/play.py
@@ -1,3 +1,8 @@
+# name : Alireza Nejati
+# gmail address : alirezanejatiz27@gmail.com
+# github ID : Alireza-njt
+# last submit : Tuesday, July 15, 2025 2:54 AM +0330
+
 from nim import train, play
 
 ai = train(10000)
